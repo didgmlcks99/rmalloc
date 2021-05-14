e5298b1142e01cb56f3c96294a213e4292c80fe8
didgmlcks99 <heechy06@naver.com>
2021-05-14 13:36:59 +0900

    Best Fit Positive

--- rmalloc.c
+++ rmalloc.c
@@ -35,7 +35,7 @@ void * rmalloc (size_t s)
 				used->next = temp;
 				return (void *)temp + sizeof(rm_header);
 			}
-			// when found the first chunk the fits user's request with remaining space
+			// when found the first chunk the fits user's request
 			else if(s <= tot_size){
 				rm_header_ptr free2 = &rm_free_list;
 				size_t coal_size = free2->size;
@@ -49,12 +49,13 @@ void * rmalloc (size_t s)
 					// deallocate temp
 					munmap(temp2, (temp2->size + sizeof(rm_header)));
 				}
-				
-				// allocate for second part of memory after split
-				rm_header_ptr split = mmap(NULL, ((coal_size-s) + sizeof(rm_header)), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
-				split->next = free2->next;
-				split->size = coal_size - s;
-				free2->next = split;
+				if(s != tot_size){
+					// allocate for second part of memory after split
+					rm_header_ptr split = mmap(NULL, ((coal_size-s) + sizeof(rm_header)), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+					split->next = free2->next;
+					split->size = coal_size - s;
+					free2->next = split;
+				}
 
 				// send code to allocation and connection to used list at user request amount
 				break;
@@ -75,6 +76,92 @@ void * rmalloc (size_t s)
 		new->size = s;
 		used->next = new;
 		return (void *)new + sizeof(rm_header);
+	}
+	// best fit policy case
+	else if(policy == BestFit){
+		size_t min_fit = 1000000;
+		rm_header_ptr points_min_chunk;
+
+		rm_header_ptr free = &rm_free_list;
+		while(free->next != 0x0){
+			tot_size += free->next->size;
+			if((free->next->size - s) >= 0 && (free->next->size - s) < min_fit){
+				min_fit = (free->next->size - s);
+				points_min_chunk = free;
+				if(min_fit == 0){
+					// bring target data to temporary
+					rm_header_ptr temp = free->next;
+					// connect free list to the next node after target node
+					free->next = free->next->next;
+					
+					rm_header_ptr used = &rm_used_list;
+					// walks through used list to meet the end place
+					while(used->next != 0x0){
+						used = used->next;
+					}
+					// add target address at the end of used list
+					temp->next = 0x0;
+					used->next = temp;
+					return (void *)temp + sizeof(rm_header);
+				}
+			}
+			free = free->next;
+		}
+		printf("%lu : %lu\n", min_fit, tot_size);
+		if(min_fit != 1000000){
+			// bring target node to temp
+			rm_header_ptr temp = points_min_chunk->next;
+			// current node pointer changes to the node after the target node
+			points_min_chunk->next = points_min_chunk->next->next;
+			// deallocate target node
+			munmap(temp, (temp->size) + sizeof(rm_header));
+
+			// allocate for second part of memory after split
+			rm_header_ptr split = mmap(NULL, ((min_fit) + sizeof(rm_header)), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+			split->next = points_min_chunk->next;
+			split->size = min_fit;
+			points_min_chunk->next = split;
+		}else if(s <= tot_size){
+			rm_header_ptr free2 = &rm_free_list;
+			size_t coal_size = free2->size;
+			while(coal_size != tot_size){
+				// increment size for coalescing
+				coal_size += free2->next->size;
+				// bring first node to temp
+				rm_header_ptr temp2 = free2->next;
+				// connect current node to the node after target node
+				free2->next = free2->next->next;
+				// deallocate temp
+				munmap(temp2, (temp2->size + sizeof(rm_header)));
+			}
+			
+			if(s != tot_size){
+				// allocate for second part of memory after split
+				rm_header_ptr split = mmap(NULL, ((coal_size-s) + sizeof(rm_header)), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+				split->next = free2->next;
+				split->size = coal_size - s;
+				free2->next = split;
+			}
+		}
+		// when there are no feasible memory region for user's request
+		rm_header_ptr used = &rm_used_list;
+		// walks through used list to meet the end place
+		while(used->next != 0x0){
+			used = used->next;
+		}
+
+		// add new allocated header to the end of used list
+		rm_header_ptr new;
+		new = mmap(NULL, (s+sizeof(rm_header)), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+		new->next = 0x0;
+		new->size = s;
+		used->next = new;
+		return (void *)new + sizeof(rm_header);
+	}
+	// worst fit policy case
+	else if(policy == WorstFit){
+		printf("Worstfit\n");
+		return 0x0;
 	}
 	return 0x0;
 }
@@ -118,7 +205,8 @@ void rmshrink ()
 
 void rmconfig (rm_option opt) 
 {
-	// TODO
+	// make global variable policy to user request option
+	policy = opt;
 }
 
 
